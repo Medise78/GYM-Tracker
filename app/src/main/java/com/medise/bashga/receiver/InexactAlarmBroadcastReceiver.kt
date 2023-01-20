64e3deeaed0f37fce062b57c2b343ea7a3b4357e
medise <medise08@gmail.com>
2023-01-21 01:01:42 +0330

    add-notification

--- app/src/main/java/com/medise/bashga/receiver/InexactAlarmBroadcastReceiver.kt
+++ app/src/main/java/com/medise/bashga/receiver/InexactAlarmBroadcastReceiver.kt
@@ -4,6 +4,7 @@ import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import com.medise.bashga.data.repository.GymRepository
+import com.medise.bashga.util.DateConverterToPersian
 import com.medise.bashga.util.showNotification
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.CoroutineScope
@@ -11,6 +12,7 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.launchIn
 import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.launch
+import java.time.LocalDate
 import javax.inject.Inject
 
 private const val NOTIFICATION_ID = 1002
@@ -24,16 +26,33 @@ class InexactAlarmBroadcastReceiver : BroadcastReceiver() {
     @Inject
     lateinit var repository: GymRepository
 
+    private val date = DateConverterToPersian()
+
     override fun onReceive(context: Context, intent: Intent) {
+
+        date.GregorianToPersian(
+            LocalDate.now().year,
+            LocalDate.now().monthValue,
+            LocalDate.now().dayOfMonth
+        )
 
         CoroutineScope(Dispatchers.IO).launch {
             repository.getAllPerson().onEach { personEntityList ->
+                val p = personEntityList.filter {
+                    LocalDate.parse(it.endDay)?.isEqual(
+                        LocalDate.of(
+                            date.year,
+                            date.month,
+                            date.day
+                        )
+                    ) == true
+                }
                 showNotification(
                     context,
                     NOTIFICATION_CHANNEL_ID,
                     NOTIFICATION_CHANNEL_NAME,
                     NOTIFICATION_ID,
-                    personEntityList.map { it.name?:"" },
+                    p.map { it.name?:"" },
                     personEntityList.size
                 )
             }.launchIn(this)
